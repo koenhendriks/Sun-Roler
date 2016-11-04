44ed4cd458dda1fa5d5a13ca61dd336624880b26
Koen Hendriks <info@koenhendriks.com>
2016-11-04 14:50:45 +0100

    Added init for bootstrap template

--- Central/Central/settings.py
+++ Central/Central/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CentralUnit.apps.CentralunitConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
