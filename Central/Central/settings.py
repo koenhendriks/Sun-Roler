d6d6826a2ad62a14420e90d2ad5c632086fa7426
Koen Hendriks <info@koenhendriks.com>
2016-11-10 09:41:37 +0100

    Updated settings and added static jquery

--- Central/Central/settings.py
+++ Central/Central/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5k%v+=(11ax8q4)sc4%rb)&p=0jnm^byxkz*pu1e3)tyyxglr!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.70']
+ALLOWED_HOSTS = ['192.168.1.70','127.0.0.1','localhost']
 
 
 # Application definition
