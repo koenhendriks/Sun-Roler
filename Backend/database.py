5ef783f3ee8b8013b5be587b5ceea301221165ac
Jim-David Bennink <ik.ben.van.een.party@gmail.com>
2016-11-12 12:56:32 +0100

    Added exception handling and fix some minor things.

--- Backend/database.py
+++ Backend/database.py
@@ -1,3 +1,4 @@
+from sqlite3 import DatabaseError
 import sqlite3
 import time
 
@@ -37,7 +38,7 @@ class DB:
             c: Cursor database connection object.
 
         """
-        conn = sqlite3.connect('control_unit.db')
+        conn = sqlite3.connect('control_unit.sqlite3')
         conn.row_factory = self.json_return
         c = conn.cursor()
         return conn, c
@@ -59,73 +60,100 @@ class DB:
         Insert specified sensor names in table 'sensor_settings' to be able to store sensor settings.
         Insert default roll in and roll out distances.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
+        try:
+            conn, c = self.open()
 
-        # Specify sensor identification, name, default minimal value and default maximum value.
-        sensors = {1: ['anemometer', 0, 0],
-                   2: ['rain', 0, 0],
-                   3: ['temperature', 0, 0],
-                   4: ['humidity', 0, 0],
-                   5: ['light', 0, 0]}
+            # Specify sensor identification, name, default minimal value and default maximum value.
+            sensors = {1: ['anemometer', 0, 0],
+                       2: ['rain', 0, 0],
+                       3: ['temperature', 0, 0],
+                       4: ['humidity', 0, 0],
+                       5: ['light', 0, 0]}
 
-        c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {s_fn} {s_ft}, {s_n_fn} {s_n_ft}, {s_v_fn} {s_v_ft})"
-                  .format(tn='sensor_settings',
-                          id_fn='ID', id_ft='INTEGER PRIMARY KEY AUTOINCREMENT',
-                          s_fn='sensor', s_ft='INTEGER',
-                          s_n_fn='setting_name', s_n_ft='TEXT',
-                          s_v_fn='setting_value', s_v_ft='TEXT'))
-
-        c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {m_fn} {m_ft}, {t_fn} {t_ft})"
-                  .format(tn='log',
-                          id_fn='ID', id_ft='INTEGER PRIMARY KEY AUTOINCREMENT',
-                          m_fn='message', m_ft='TEXT',
-                          t_fn='log_time', t_ft='INTEGER'))
-
-        # Default roll in distance
-        insert_values = (0, 'roll_in_distance', 10)
-        c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
-                  insert_values)
-
-        # Default roll out distance
-        insert_values = (0, 'roll_out_distance', 40)
-        c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
-                  insert_values)
-
-        for key, value in sensors.items():
-            c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {sv_fn} {sv_ft}, {rt_fn} {rt_ft})"
-                      .format(tn=value[0],
+            c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {s_fn} {s_ft}, {s_n_fn} {s_n_ft}, {s_v_fn} {s_v_ft})"
+                      .format(tn='sensor_settings',
                               id_fn='ID', id_ft='INTEGER PRIMARY KEY AUTOINCREMENT',
-                              sv_fn='sensor_value', sv_ft='INTEGER',
-                              rt_fn='reading_time', rt_ft='INTEGER'))
+                              s_fn='sensor', s_ft='INTEGER',
+                              s_n_fn='setting_name', s_n_ft='TEXT',
+                              s_v_fn='setting_value', s_v_ft='TEXT'))
 
-            insert_values = (key, 'sensor_name', value[0])
+            c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {m_fn} {m_ft}, {t_fn} {t_ft})"
+                      .format(tn='log',
+                              id_fn='ID', id_ft='INTEGER PRIMARY KEY AUTOINCREMENT',
+                              m_fn='message', m_ft='TEXT',
+                              t_fn='log_time', t_ft='INTEGER'))
+
+            # Default roll in distance
+            insert_values = (0, 'roll_in_distance', 10)
             c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
                       insert_values)
 
-            insert_values = (key, 'min_value', value[1])
+            # Default roll out distance
+            insert_values = (0, 'roll_out_distance', 40)
             c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
                       insert_values)
 
-            insert_values = (key, 'max_value', value[2])
-            c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
-                      insert_values)
+            for key, value in sensors.items():
+                c.execute("CREATE TABLE {tn} ({id_fn} {id_ft}, {sv_fn} {sv_ft}, {rt_fn} {rt_ft})"
+                          .format(tn=value[0],
+                                  id_fn='ID', id_ft='INTEGER PRIMARY KEY AUTOINCREMENT',
+                                  sv_fn='sensor_value', sv_ft='INTEGER',
+                                  rt_fn='reading_time', rt_ft='INTEGER'))
 
-        self.close(conn)
+                insert_values = (key, 'sensor_name', value[0])
+                c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
+                          insert_values)
 
-    def insert_sensor_value(self, sensor, value):
+                insert_values = (key, 'min_value', value[1])
+                c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
+                          insert_values)
+
+                insert_values = (key, 'max_value', value[2])
+                c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
+                          insert_values)
+                return '200'
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
+
+    def insert_sensor_value(self, sensor_name, value):
         """ Insert sensor values in database.
 
         Args:
-            sensor: Name of sensor to store value of.
+            sensor_name: Name of sensor to store value of.
             value: Value of reading to store.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
-        insert_values = (value, int(time.time()))
-        c.execute("INSERT INTO {s} (sensor_value, reading_time) VALUES (?, ?)".format(
-            s=sensor), insert_values)
-        self.close(conn)
+        try:
+            conn, c = self.open()
+            insert_values = (value, int(time.time()))
+            c.execute("INSERT INTO {s} (sensor_value, reading_time) VALUES (?, ?)".format(
+                s=sensor_name), insert_values)
+            return '200'
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def select_last_sensor_value(self, sensor_name):
         """ Select last sensor value from database.
@@ -133,22 +161,32 @@ class DB:
         Args:
             sensor_name: Name of sensor to select value from.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
         Returns:
-            zero: Return zero (0) if not rows fetched.
+            zero: Return sensor_value zero (0) if no rows fetched.
             fetched_rows: The fetched rows from the database containing the sensor value.
+            500: DatabaseError was raised. return error 500.
 
         """
 
-        conn, c = self.open()
-        c.execute("SELECT sensor_value FROM {tn} ORDER BY reading_time DESC LIMIT 1".format(
-            tn=sensor_name))
-        fetched_rows = c.fetchall()
-        self.close(conn)
-        if fetched_rows:
-            return fetched_rows
-        else:
-            # If no reading found, return zero.
-            return [{'sensor_value': 0}]
+        try:
+            conn, c = self.open()
+            c.execute("SELECT sensor_value FROM {tn} ORDER BY reading_time DESC LIMIT 1".format(
+                tn=sensor_name))
+            fetched_rows = c.fetchall()
+            if fetched_rows:
+                return fetched_rows
+            else:
+                # If no reading found, return zero.
+                return [{'sensor_value': 0}]
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def select_sensor_values(self, sensor_name, start_time, end_time=''):
         """ Select sensor values from database.
@@ -158,34 +196,55 @@ class DB:
             start_time: Left boundary from time period to return values from.
             end_time: Right boundary from time period to return values from.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
         Returns:
             fetched_rows: The fetched rows from the database containing the sensor values.
+            500: DatabaseError was raised. return error 500.
 
         """
 
-        # Check if end_time is given or not.
-        if not end_time:
-            end_time = "''"
+        try:
+            # Check if end_time is given or not.
+            if not end_time:
+                end_time = "''"
 
-        conn, c = self.open()
-        c.execute("SELECT sensor_value FROM {tn} WHERE reading_time BETWEEN {ts} AND {te}".format(
-            tn=sensor_name, ts=start_time, te=end_time))
-        fetched_rows = c.fetchall()
-        self.close(conn)
-        return fetched_rows
+            conn, c = self.open()
+            c.execute("SELECT sensor_value FROM {tn} WHERE reading_time BETWEEN {ts} AND {te}".format(
+                tn=sensor_name, ts=start_time, te=end_time))
+            fetched_rows = c.fetchall()
+            return fetched_rows
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def select_sensor_ids_names(self):
         """ Select names and id's from all sensors from database.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
         Returns:
             fetched_rows: The fetched rows from the database containing the names and the id's of the sensors.
+            500: DatabaseError was raised. return error 500.
 
         """
-        conn, c = self.open()
-        c.execute("SELECT sensor, setting_value FROM sensor_settings WHERE setting_name = 'sensor_name'")
-        fetched_rows = c.fetchall()
-        self.close(conn)
-        return fetched_rows
+
+        try:
+            conn, c = self.open()
+            c.execute("SELECT sensor, setting_value FROM sensor_settings WHERE setting_name = 'sensor_name'")
+            fetched_rows = c.fetchall()
+            return fetched_rows
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def delete_sensor_values(self, sensor_name, start_time, end_time=''):
         """ Delete sensor values from database.
@@ -195,16 +254,29 @@ class DB:
             start_time: Left boundary from time period to delete values from.
             end_time: Right boundary from time period to delete values from.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
+        try:
+            # Check if end_time is given or not.
+            if not end_time:
+                end_time = "''"
 
-        # Check if end_time is given or not.
-        if not end_time:
-            end_time = "''"
+            conn, c = self.open()
+            c.execute("DELETE FROM {tn} WHERE reading_time BETWEEN {ts} AND {te}".format(
+                tn=sensor_name, ts=start_time, te=end_time))
+            return '200'
 
-        conn, c = self.open()
-        c.execute("DELETE FROM {tn} WHERE reading_time BETWEEN {ts} AND {te}".format(
-            tn=sensor_name, ts=start_time, te=end_time))
-        self.close(conn)
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def select_sensor_setting(self, sensor_id, setting_name):
         """ Select setting from sensor_setting table.
@@ -213,13 +285,26 @@ class DB:
             sensor_id: ID of sensor.
             setting_name: Name of setting to select value from.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            fetched_rows: The fetched rows from the database containing the sensor setting value.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
-        c.execute("SELECT setting_value FROM sensor_settings WHERE sensor = {s} AND setting_name = '{sn}'"
-                  .format(s=sensor_id, sn=setting_name))
-        fetched_rows = c.fetchall()
-        self.close(conn)
-        return fetched_rows
+        try:
+            conn, c = self.open()
+            c.execute("SELECT setting_value FROM sensor_settings WHERE sensor = {s} AND setting_name = '{sn}'"
+                      .format(s=sensor_id, sn=setting_name))
+            fetched_rows = c.fetchall()
+            return fetched_rows
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def insert_sensor_setting(self, sensor_id, setting_name, setting_value):
         """ Insert setting into sensor_setting table.
@@ -229,11 +314,26 @@ class DB:
             setting_name: Name of setting to select value from.
             setting_value: The value to store.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
-        insert_values = (sensor_id, setting_name, setting_value)
-        c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)", insert_values)
-        self.close(conn)
+        try:
+            conn, c = self.open()
+            insert_values = (sensor_id, setting_name, setting_value)
+            c.execute("INSERT INTO sensor_settings (sensor, setting_name, setting_value) VALUES (?, ?, ?)",
+                      insert_values)
+            return '200'
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def update_sensor_setting(self, sensor_id, setting_name, setting_value):
         """ Update setting from sensor_setting table.
@@ -243,11 +343,25 @@ class DB:
             setting_name: Name of setting to update value from.
             setting_value: The new value to store.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
-        c.execute("UPDATE sensor_settings SET setting_value = {sv} WHERE setting_name = '{sn}' AND sensor = {s}".format(
-            sv=setting_value, sn=setting_name, s=sensor_id))
-        self.close(conn)
+        try:
+            conn, c = self.open()
+            c.execute("UPDATE sensor_settings SET setting_value = {sv} WHERE setting_name = '{sn}' AND sensor = {s}".format(
+                sv=setting_value, sn=setting_name, s=sensor_id))
+            return '200'
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def insert_log_message(self, message):
         """ Insert log message into database.
@@ -255,11 +369,25 @@ class DB:
         Args:
             message: Message to insert into database.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
-        conn, c = self.open()
-        insert_values = (message, int(time.time()))
-        c.execute("INSERT INTO log (message, log_time) VALUES (?, ?)", insert_values)
-        self.close(conn)
+        try:
+            conn, c = self.open()
+            insert_values = (message, int(time.time()))
+            c.execute("INSERT INTO log (message, log_time) VALUES (?, ?)", insert_values)
+            return '200'
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def delete_log_messages(self, start_time, end_time=''):
         """ Delete log messages from database.
@@ -268,27 +396,52 @@ class DB:
             start_time: Left boundary from time period to delete values from.
             end_time: Right boundary from time period to delete values from.
 
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
+
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
         """
+        try:
+            # Check if end_time is given or not.
+            if not end_time:
+                end_time = "''"
 
-        # Check if end_time is given or not.
-        if not end_time:
-            end_time = "''"
+            conn, c = self.open()
+            c.execute("DELETE FROM log WHERE log_time BETWEEN {ts} AND {te}".format(
+                ts=start_time, te=end_time))
+            return '200'
 
-        conn, c = self.open()
-        c.execute("DELETE FROM log WHERE log_time BETWEEN {ts} AND {te}".format(
-            ts=start_time, te=end_time))
-        self.close(conn)
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
     def select_log_messages(self):
         """ Select all log messages from database.
 
-        """
-        conn, c = self.open()
-        c.execute("SELECT * FROM log")
-        fetched_rows = c.fetchall()
-        self.close(conn)
-        return fetched_rows
+        Raises:
+            DatabaseError: Something went wrong by opening a database connection or manipulating the database.
 
+        Returns:
+            200: Statements were executed successfully.
+            500: DatabaseError was raised. return error 500.
+
+        """
+        try:
+            conn, c = self.open()
+            c.execute("SELECT * FROM log")
+            fetched_rows = c.fetchall()
+            return fetched_rows
+
+        except DatabaseError:
+            return '500'
+
+        finally:
+            self.close(conn)
 
 if __name__ == "__main__":
     # Initialize database when this script is being called directly.
