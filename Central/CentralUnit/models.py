336651a4e2a7c2569b7ef0d410e38c26358d341e
Koen Hendriks <info@koenhendriks.com>
2016-11-15 18:37:57 +0100

    Updated table name to meta and added __str__ value for admin

--- Central/CentralUnit/models.py
+++ Central/CentralUnit/models.py
@@ -4,55 +4,90 @@ from django.db import models
 # Create your models here.
 
 class Anemometer(models.Model):
-    db_table = 'anemometer'
+    class Meta:
+        db_table = 'anemometer'
+
     ID = models.IntegerField(primary_key=True)
     sensor_value = models.IntegerField()
     screen_position = models.IntegerField()
     reading_time = models.IntegerField()
+
+    def __str__(self):
+        return str(self.sensor_value)+'℃ at '+str(self.reading_time)
 
 
 class Humidity(models.Model):
-    db_table = 'humidity'
+    class Meta:
+        db_table = 'humidity'
+
     ID = models.IntegerField(primary_key=True)
     sensor_value = models.IntegerField()
     screen_position = models.IntegerField()
     reading_time = models.IntegerField()
+
+    def __str__(self):
+        return str(self.sensor_value)+' at '+str(self.reading_time)
 
 
 class Light(models.Model):
-    db_table = 'light'
+    class Meta:
+        db_table = 'light'
+
     ID = models.IntegerField(primary_key=True)
     sensor_value = models.IntegerField()
     screen_position = models.IntegerField()
     reading_time = models.IntegerField()
+
+    def __str__(self):
+        return str(self.sensor_value)+'lx at '+str(self.reading_time)
 
 
 class Rain(models.Model):
-    db_table = 'rain'
+    class Meta:
+        db_table = 'rain'
+
     ID = models.IntegerField(primary_key=True)
     sensor_value = models.IntegerField()
     screen_position = models.IntegerField()
     reading_time = models.IntegerField()
+
+    def __str__(self):
+        return str(self.sensor_value)+' at '+str(self.reading_time)
 
 
 class Temperature(models.Model):
-    db_table = 'temperature'
+    class Meta:
+        db_table = 'temperature'
+
     ID = models.IntegerField(primary_key=True)
     sensor_value = models.IntegerField()
     screen_position = models.IntegerField()
     reading_time = models.IntegerField()
 
+    def __str__(self):
+        return str(self.sensor_value)+' at '+str(self.reading_time)
+
 
 class Log(models.Model):
-    db_table = 'log'
+    class Meta:
+        db_table = 'log'
+
     ID = models.IntegerField(primary_key=True)
     message = models.TextField()
     log_time = models.IntegerField()
 
+    def __str__(self):
+        return self.message
+
 
 class SensorSettings(models.Model):
-    db_table = 'sensor_settings'
+    class Meta:
+        db_table = 'sensor_settings'
+
     ID = models.IntegerField(primary_key=True)
     sensor = models.IntegerField()
     setting_name = models.TextField()
     setting_value = models.TextField()
+
+    def __str__(self):
+        return self.setting_name+' : '+self.setting_value
