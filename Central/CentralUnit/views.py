cf4aff9a38e2b173bfa360d2c27256134e674ce6
Koen Hendriks <info@koenhendriks.com>
2016-11-15 18:51:20 +0100

    Created database reading of temperature to show in view

--- Central/CentralUnit/views.py
+++ Central/CentralUnit/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Temperature
+
 
 def index(request):
-    context = {}
+    context = dict()
+    latest_temp = Temperature.objects.order_by('-reading_time')[:1]
+    context['temperature'] = [t.sensor_value for t in latest_temp][0]
     return render(request, 'CentralUnit/index.html', context)
