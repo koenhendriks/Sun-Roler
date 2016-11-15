184e57c1a7b111f608138b60111f8ffc2e229896
Koen Hendriks <info@koenhendriks.com>
2016-11-15 18:38:35 +0100

    Added some values and registered models to Admin

--- Central/CentralUnit/admin.py
+++ Central/CentralUnit/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Anemometer, Humidity, Light, Rain, Temperature, Log, SensorSettings
+
+admin.site.register(Anemometer)
+admin.site.register(Humidity)
+admin.site.register(Light)
+admin.site.register(Rain)
+admin.site.register(Temperature)
+admin.site.register(Log)
+admin.site.register(SensorSettings)
 # Register your models here.
