ad04d9fb63b0048db688a04a0ba67bb1f95e82f9
Koen Hendriks <info@koenhendriks.com>
2016-11-15 18:09:25 +0100

    init of models

--- Central/CentralUnit/models.py
+++ Central/CentralUnit/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Anemometer(models.Model):
+    db_table = '"anemometer"'
+    ID = models.IntegerField(primary_key=True)
+    sensor_value = models.IntegerField()
+    screen_position = models.IntegerField()
+    reading_time = models.IntegerField()
+
+
+class Humidity(models.Model):
+    db_table = '"humidity"'
+    ID = models.IntegerField(primary_key=True)
+    sensor_value = models.IntegerField()
+    screen_position = models.IntegerField()
+    reading_time = models.IntegerField()
+
+
+class Light(models.Model):
+    db_table = '"light"'
+    ID = models.IntegerField(primary_key=True)
+    sensor_value = models.IntegerField()
+    screen_position = models.IntegerField()
+    reading_time = models.IntegerField()
+
+
+class Rain(models.Model):
+    db_table = '"rain"'
+    ID = models.IntegerField(primary_key=True)
+    sensor_value = models.IntegerField()
+    screen_position = models.IntegerField()
+    reading_time = models.IntegerField()
+
+
+class Temperature(models.Model):
+    db_table = '"temperature"'
+    ID = models.IntegerField(primary_key=True)
+    sensor_value = models.IntegerField()
+    screen_position = models.IntegerField()
+    reading_time = models.IntegerField()
+
+class Log(models.Model):
+    db_table = '"log"'
+    ID = models.IntegerField(primary_key=True)
+    message = models.TextField()
+    log_time = models.IntegerField()
+
+class SensorSettings(models.Model):
+    db_table = '"sensor_settings"'
+    ID = models.IntegerField(primary_key=True)
+    sensor = models.IntegerField()
+    setting_name = models.TextField()
+    setting_value = models.TextField()
