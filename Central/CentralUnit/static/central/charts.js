584333bb9fbf618e8bcaeb8bd42e39d1c23faa87
Koen Hendriks <info@koenhendriks.com>
2016-11-16 15:32:18 +0100

    Hardcoded highchart example for temperature

---
+++ Central/CentralUnit/static/central/charts.js
@@ -0,0 +1,52 @@
+$(function () {
+    Highcharts.chart('temperature-chart', {
+        chart: {
+            type: 'area',
+            width: 320,
+            height: 200
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['1479305312', '1479305372', '1479305432', '1479305492', '1479305552', '1479305612', '1479305672'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            min: 15,
+            title: {
+                text: '℃',
+                rotation: 0
+            },
+            labels: {
+
+                formatter: function () {
+                    return this.value;
+                }
+            }
+        },
+        tooltip: {
+            split: true,
+            valueSuffix: ' millions',
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [{
+            showInLegend: false,
+            data: [20, 21, 20, 18, 19]
+        }]
+    });
+});
