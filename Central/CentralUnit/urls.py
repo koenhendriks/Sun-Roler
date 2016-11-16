5718de8cd2bee2780a1543b7b8f0de6e81330ce6
Koen Hendriks <info@koenhendriks.com>
2016-11-16 10:59:37 +0100

    Updated API to have all sensors in 1 request

--- Central/CentralUnit/urls.py
+++ Central/CentralUnit/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('temperature', views.temperature, name='index'),
-    url('light', views.light, name='index'),
+    url('sensors', views.sensors, name='index'),
     url(r'^$', views.index, name='index'),
 ]
--- Central/CentralUnit/views.py
+++ Central/CentralUnit/views.py
@@ -5,18 +5,20 @@ from .models import Temperature, Light
 
 def index(request):
     context = dict()
+
     latest_temp = Temperature.objects.order_by('-reading_time')[:1]
+    latest_light = Light.objects.order_by('-reading_time')[:1]
     context['temperature'] = [t.sensor_value for t in latest_temp][0]
+    context['light'] = [l.sensor_value for l in latest_light][0]
     return render(request, 'CentralUnit/index.html', context)
 
 
-def temperature(request):
+def sensors(request):
     latest_temp = Temperature.objects.order_by('-reading_time')[:1]
     current_temperature = [t.sensor_value for t in latest_temp][0]
-    return JsonResponse({'temperature': current_temperature})
-
-
-def light(request):
     latest_light = Light.objects.order_by('-reading_time')[:1]
-    current_light = [t.sensor_value for t in latest_light][0]
-    return JsonResponse({'light': current_light})
+    current_light = [l.sensor_value for l in latest_light][0]
+    return JsonResponse({
+        'light': current_light,
+        'temperature' : current_temperature
+    })
