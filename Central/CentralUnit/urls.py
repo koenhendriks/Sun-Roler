f4eeea1f70b8098405355d7cd4822b998d4774d3
Koen Hendriks <info@koenhendriks.com>
2016-11-16 10:54:18 +0100

    Build Json API for sensors

--- Central/CentralUnit/urls.py
+++ Central/CentralUnit/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('temperature', views.temperature, name='index'),
+    url('light', views.light, name='index'),
     url(r'^$', views.index, name='index'),
 ]
--- Central/CentralUnit/views.py
+++ Central/CentralUnit/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
-from .models import Temperature
+from .models import Temperature, Light
 
 
 def index(request):
@@ -8,3 +8,15 @@ def index(request):
     latest_temp = Temperature.objects.order_by('-reading_time')[:1]
     context['temperature'] = [t.sensor_value for t in latest_temp][0]
     return render(request, 'CentralUnit/index.html', context)
+
+
+def temperature(request):
+    latest_temp = Temperature.objects.order_by('-reading_time')[:1]
+    current_temperature = [t.sensor_value for t in latest_temp][0]
+    return JsonResponse({'temperature': current_temperature})
+
+
+def light(request):
+    latest_light = Light.objects.order_by('-reading_time')[:1]
+    current_light = [t.sensor_value for t in latest_light][0]
+    return JsonResponse({'light': current_light})
